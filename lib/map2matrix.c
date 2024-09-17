44dd3ef1e0c3ab5c39a0b9e4ab4222cdda429975
Hamza <hmz@example.com>
2024-09-17 17:43:37 +0100

    + 2 files

---
+++ lib/map2matrix.c
@@ -0,0 +1,6 @@
+// convert the map to a matrix
+//use create_matrix
+#include "lib.h"
+
+//strlen ster tani (lwl mora dak li fih l info) = cols
+//create_char_matrix(r , )
---
+++ lib/necessery_checks.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   necessery_checks.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haitouna <haitouna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 17:32:13 by haitouna          #+#    #+#             */
+/*   Updated: 2024/09/17 17:35:51 by haitouna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// function to convert map into matrix
+
+int	map_to_matrix(char *map, int row, int col)
+{
+	char	*temp_matrix;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	int	map_index = 0;
+
+	temp_matrix = (int **)malloc(sizeof(int *)* row)
+}
